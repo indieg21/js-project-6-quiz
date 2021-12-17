74454310bef9e781f3fcb16e5bfc77d476eace41
indieg21 <Indiagraham21@gmail.com>
2021-12-17 08:57:30 -0500

    refactor the quiz app

--- script.js
+++ script.js
@@ -1,69 +1,41 @@
-let answerList = ["New York City", "Paris", "Five"];
+let correctAnswer = ["New York City", "Paris", "Five"]; // all correct answers are stored in sequence order in an array
 
-let answer1 = document.querySelectorAll(".answer1");
+let answer1 = document.querySelectorAll(".answer1"); // here is each input box selected.
 let answer2 = document.querySelectorAll(".answer2");
 let answer3 = document.querySelectorAll(".answer3");
 
-let result1 = document.querySelector(".result1");
-let result2 = document.querySelector(".result2");
-let result3 = document.querySelector(".result3");
+let quiz = document.querySelector(".quiz"); // selecting the form field
+let displayResult = document.querySelector(".displayResult"); // div to display the result answer
 
-function checkAnswer1() {
-  let answer = undefined;
-  console.log(answer1);
-  answer1.forEach((answerElement) => {
-    console.log(answerElement);
+quiz.addEventListener("submit", (e) => { // this is form submission
+  e.preventDefault();
+  console.log(quiz);
+  let userSelection = [ // Array to store all answers. This is how to find the correct answers
+    quiz.question1.value, //radio button values. Each selected radio buttons are here
+    quiz.question2.value,
+    quiz.question3.value,
+  ];
 
-    if (answerElement.checked) {
-      answer = answerElement.value;
+  let result = ""; // empty variable to add the results
+  userSelection.forEach((answer, index) => { //looping through the array of selected answers
+    if (answer === correctAnswer[index]) { // check through each answers true or false for results
+      result += `<h3>You answer is correct for question ${ 
+        index + 1
+      } : ${answer}</h3>`; // if the answer is correct the correct answer will be displayed
+    } else {
+      result += `<h3>You have selected the worng answer and 
+    the correct answer for question ${index + 1} is : ${
+        correctAnswer[index]
+      }</h3>`; // if the answer is incorrect the correct answer will be displayed
     }
   });
+  displayResult.innerHTML = result; // to reset the page after submit and getting answer results 
+  let btn = document.createElement("button");
+  btn.textContent = "Reset";
+  btn.onclick = resetQuiz;
+  displayResult.appendChild(btn);
+});
 
-  // let element = document.createElement("h3");
-  if (answer === answerList[0]) {
-    result1.innerHTML = `
-            <h3>You answer is correct : ${answer}</h3>
-    `;
-  } else {
-    result1.innerHTML = `
-    <h3> You have selected the worng answer:
-    The correct answer is ${answerList[0]} </h3>`;
-  }
-}
-
-function checkAnswer2() {
-  let answer = undefined;
-  answer2.forEach((answerElement) => {
-    if (answerElement.checked) {
-      answer = answerElement.value;
-    }
-  });
-
-  if (answer === answerList[1]) {
-    result2.innerHTML = `<h3>You answer is correct : ${answer}</h3>`;
-  } else {
-    result2.innerHTML = `
-    <h3>You have selected the worng answer and 
-    the correct answer is ${answerList[1]}</h3>`;
-  }
-}
-
-function checkAnswer3() {
-  let answer = undefined;
-  answer3.forEach((answerElement) => {
-    if (answerElement.checked) {
-      answer = answerElement.value;
-    }
-  });
-
-  if (answer === answerList[2]) {
-    result3.innerHTML = `
-    <h3>You answer is correct : ${answer}</h3>
-    `;
-  } else {
-    result3.innerHTML = `
-    <h3>You have selected the worng answer and
-    The correct answer is ${answerList[2]}</h3>   
-`;
-  }
+function resetQuiz() {
+  location.reload(); // fucntion to reload same page. Javascript built in fucntion
 }
